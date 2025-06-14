fee0aa155b872f994c24215a5b91da45ba2b2118
DefaiderST178 <dbelogorskiy@gmail.com>
2025-06-14 18:21:50 +0300

    Update 14/06/2025 Update UX in AllMedicationView, Added clusters (tablets, drops, solutions, and others) for the drug repository.

--- View/AllMedications.xaml.cs
+++ View/AllMedications.xaml.cs
@@ -12,8 +12,31 @@ public partial class AllMedications : ContentPage
         BindingContext = medicationViewModel;
     }
 
-	// выбо препарата
-    private void medicationList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    // переход к типу препарата
+    private void OnGoToSubheader_Clicked(object sender, EventArgs e)
+    {
+        if (subheaderPicker.SelectedIndex != -1)
+        {
+            switch (subheaderPicker.SelectedItem.ToString())
+            {
+                case "“аблетки":
+                    scrollView.ScrollToAsync(subheader1, ScrollToPosition.Start, true);
+                    break;
+                case " апли":
+                    scrollView.ScrollToAsync(subheader2, ScrollToPosition.Start, true);
+                    break;
+                case "–астворы":
+                    scrollView.ScrollToAsync(subheader3, ScrollToPosition.Start, true);
+                    break;
+                case "ƒругое":
+                    scrollView.ScrollToAsync(subheader4, ScrollToPosition.Start, true);
+                    break;
+            }
+        }
+    }
+
+    // выбор таблетки из списка 
+    private void pillsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
         if (e.SelectedItem == null)
             return;
@@ -22,6 +45,45 @@ public partial class AllMedications : ContentPage
 
         Navigation.PushAsync(new SelectedMedication(selectedMedication));
 
-        medicationList.SelectedItem = null;
+        pillsList.SelectedItem = null;
+    }
+
+    // выбор капель из списка 
+    private void dropsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem == null)
+            return;
+
+        var selectedMedication = e.SelectedItem as Medication;
+
+        Navigation.PushAsync(new SelectedMedication(selectedMedication));
+
+        dropsList.SelectedItem = null;
+    }
+
+    // выбор раствора из списка 
+    private void solutionsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem == null)
+            return;
+
+        var selectedMedication = e.SelectedItem as Medication;
+
+        Navigation.PushAsync(new SelectedMedication(selectedMedication));
+
+        solutionsList.SelectedItem = null;
+    }
+
+    // выбор других из списка 
+    private void otherList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem == null)
+            return;
+
+        var selectedMedication = e.SelectedItem as Medication;
+
+        Navigation.PushAsync(new SelectedMedication(selectedMedication));
+
+        otherList.SelectedItem = null;
     }
 }
--- ViewModel/MedicationViewModel.cs
+++ ViewModel/MedicationViewModel.cs
@@ -7,26 +7,85 @@ namespace VetClinicBKP.ViewModel
 {
     public class MedicationViewModel : INotifyPropertyChanged
     {
-        private ObservableCollection<Medication> _medications;
-        public ObservableCollection<Medication> Medications
+        // Взаимодейстие с кластером под таблетки
+        private ObservableCollection<Medication> _pills;
+        public ObservableCollection<Medication> Pills
         {
-            get { return _medications; }
-            set { _medications = value; OnPropertyChanged(); }
+            get { return _pills; }
+            set { _pills = value; OnPropertyChanged(); }
         }
 
-        private Medication _selectedMedication;
-        public Medication SelectedMedication
+        private Medication _selectedPill;
+        public Medication SelectedPill
         {
-            get { return _selectedMedication; }
-            set { _selectedMedication = value; OnPropertyChanged(); }
+            get { return _selectedPill; }
+            set { _selectedPill = value; OnPropertyChanged(); }
+        }
+
+        // Взаимодейстие с кластером под капли
+        private ObservableCollection<Medication> _drops;
+        public ObservableCollection<Medication> Drops
+        {
+            get { return _drops; }
+            set { _drops = value; OnPropertyChanged(); }
+        }
+
+        private Medication _selectedDrop;
+        public Medication SelectedDrop
+        {
+            get { return _selectedDrop; }
+            set { _selectedDrop = value; OnPropertyChanged(); }
+        }
+
+        // Взаимодейстие с кластером под растворы
+        private ObservableCollection<Medication> _solutions;
+        public ObservableCollection<Medication> Solutions
+        {
+            get { return _solutions; }
+            set { _solutions = value; OnPropertyChanged(); }
+        }
+
+        private Medication _selectedSolution;
+        public Medication SelectedSolution
+        {
+            get { return _selectedSolution; }
+            set { _selectedSolution = value; OnPropertyChanged(); }
+        }
+
+        // Взаимодейстие с кластером под другое
+        private ObservableCollection<Medication> _other;
+        public ObservableCollection<Medication> Other
+        {
+            get { return _other; }
+            set { _other = value; OnPropertyChanged(); }
+        }
+
+        private Medication _selectedOther;
+        public Medication SelectedOther
+        {
+            get { return _selectedOther; }
+            set { _selectedOther = value; OnPropertyChanged(); }
         }
 
         public MedicationViewModel()
-        {
-            // Получение данных из BreedRepo
+        { 
             MedicationRepo medicationRepo = new MedicationRepo();
-            List<Medication> allMedication = medicationRepo.GetAllMedications();
-            Medications = new ObservableCollection<Medication>(allMedication);
+
+            // Получение данных из репозитория (кластрер 1 - таблетки)
+            List<Medication> allPills = medicationRepo.GetAllPills();
+            Pills = new ObservableCollection<Medication>(allPills);
+
+            // Получение данных из репозитория (кластрер 2 - капли)
+            List<Medication> allDrops = medicationRepo.GetAllDrops();
+            Drops = new ObservableCollection<Medication>(allDrops);
+
+            // Получение данных из репозитория (кластрер 3 - растворы)
+            List<Medication> allSolution = medicationRepo.GetAllSolution();
+            Solutions = new ObservableCollection<Medication>(allSolution);
+
+            // Получение данных из репозитория (кластрер 4 - другое)
+            List<Medication> allOther = medicationRepo.GetAllOther();
+            Other = new ObservableCollection<Medication>(allOther);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
